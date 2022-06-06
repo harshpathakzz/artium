5edc1daf4e0277b932329fd751aae3b19db201eb
megha-pathak <meghapathak2013@gmail.com>
2022-06-06 16:01:04 +0530

    Create CartPage.js

---
+++ src/frontend/pages/CartPage/CartPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import CartProductCard from "../../components/CartProductCard/CartProductCard";
+import { CartSummary } from "../../components/CartSummary/CartSummary";
+import { useCart } from "../../contexts/cart-context";
+import "./CartPage.css";
+
+function CartPage() {
+  const { cart } = useCart();
+
+  return (
+    <div className="cart-screen">
+      <div className="cart-products">
+        {cart.map((cartProduct) => (
+          <CartProductCard key={cartProduct._id} product={cartProduct} />
+        ))}
+      </div>
+      <CartSummary />
+    </div>
+  );
+}
+
+export { CartPage };
