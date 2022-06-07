3eb67c9b2054fb3083466d1cb30c4d04d0822d18
megha-pathak <meghapathak2013@gmail.com>
2022-06-07 15:58:40 +0530

    Added more elements to the landing page

--- src/frontend/pages/Homepage/Homepage.js
+++ src/frontend/pages/Homepage/Homepage.js
@@ -1,11 +1,15 @@
 import "./Homepage.css";
 import React from "react";
 import { Hero } from "../../components/Home/Hero";
+import { AboutSection } from "../../components/AboutSection/AboutSection";
 
 function HomePage() {
   return (
     <div className="home-screen">
       <Hero />
+      <div class="secondary-section">
+        <AboutSection />
+      </div>
     </div>
   );
 }
