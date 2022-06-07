87a5833a343253a7767cfd628075d99bd58b373e
megha-pathak <meghapathak2013@gmail.com>
2022-06-07 13:09:07 +0530

    Create filtersConstants.js

---
+++ src/frontend/constants/filtersConstants.js
@@ -0,0 +1,5 @@
+export const MIN_PRICE = "MIN_PRICE";
+export const MAX_PRICE = "MAX_PRICE";
+export const SORT_BY = "SORT_BY";
+export const RATING = "RATING";
+export const CLEAR_FILTERS = "CLEAR_FILTERS";
